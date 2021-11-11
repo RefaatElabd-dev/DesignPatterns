c52586d1ad9f86e707bda55143c065bb5e9d0d4e
RefaatElabd-dev <RefaatElabd7@gmail.com>
2021-11-11 11:42:43 +0200

    Builder Pattern Added

---
+++ Builder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main0(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello").AddChild("li", "world");
+            Console.WriteLine(builder.ToString()); 
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = new PersonBuilder();
+            Person person =  builder
+                   .Lives
+                         .At("Ahmed Elzomor St.")
+                         .In("NasrCity")
+                         .WithPostCode("31418")
+                   .Works.At("National Technology")
+                         .ASA(".Net Developer")
+                         .Earns(48000);
+
+            Console.WriteLine(person);
+        }
+    }
+}
---
+++ Builder/traditionalBuilder/HtmlBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HTMLElement root = new HTMLElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HTMLElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void clear()
+        {
+            root = new HTMLElement { Name = rootName };
+        }
+
+    }
+}
